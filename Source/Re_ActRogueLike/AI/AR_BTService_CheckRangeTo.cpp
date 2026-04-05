43bd9e84776ace8ac4a419c547b9106697aafc50
ScMason90 <lwjlfmyj@outlook.com>
2026-04-05 18:44:23 +0800

    Add AR_BTService_CheckRangeTo class to check range to target(for AI character).
    Relative BP assets  of Behavior Tree and BlackBoard setup.
    
    Adjusted project settings.Lower the engine performances intend to optimize development in editor.Save costs of machine.Most were functions i don't need now.

---
+++ Source/Re_ActRogueLike/AI/AR_BTService_CheckRangeTo.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AR_BTService_CheckRangeTo.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Actor.h"
+
+void UAR_BTService_CheckRangeTo::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+	check(BBComp);
+	
+	AActor* TargetActor = Cast<AActor>(BBComp->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (IsValid(TargetActor))
+	{
+		AAIController* Controller = OwnerComp.GetAIOwner();
+		
+		APawn* OwningPawn = Controller->GetPawn();
+		check(OwningPawn)
+		
+		float DistanceTo = FVector::Dist(TargetActor->GetActorLocation(), OwningPawn->GetActorLocation());
+		bool bWithinRange = DistanceTo < MaxAttackRange;
+		
+		bool bHasLOS = Controller->LineOfSightTo(TargetActor); 
+		
+		BBComp->SetValueAsBool(WithinRangeKey.SelectedKeyName, bWithinRange && bHasLOS);
+	}
+}
--- Source/Re_ActRogueLike/Re_ActRogueLike.Build.cs
+++ Source/Re_ActRogueLike/Re_ActRogueLike.Build.cs
@@ -10,7 +10,7 @@ public class Re_ActRogueLike : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",  });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "AIModule", "Niagara" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "AIModule", "Niagara", "GameplayTasks" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
